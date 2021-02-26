5de22cc94c214c76711160162ab85cb58a56c319
Muhammad <shafiquecbl@gmail.com>
2021-02-26 12:41:08 +0500

    Feb 26 - 12 :41 PM

--- lib/screens/Home_Screen/components/Pages/Dashboard/dashboard.dart
+++ lib/screens/Home_Screen/components/Pages/Dashboard/dashboard.dart
@@ -7,6 +7,7 @@ import 'package:fyp_management/models/setData.dart';
 import 'package:fyp_management/screens/Home_Screen/components/Pages/Inbox/chat_screen.dart';
 import 'package:fyp_management/widgets/alert_dialog.dart';
 import 'package:fyp_management/widgets/customAppBar.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Dashboard extends StatefulWidget {
   @override
@@ -41,16 +42,48 @@ class _DashboardState extends State<Dashboard> {
           batch = snapshot.data[0]['Batch'];
           regNo = snapshot.data[0]['Registeration No'];
           photoURL = snapshot.data[0]['PhotoURL'];
+
+          // if group is completed then show list of supervisor //
           if (snapshot.data[1].docs.length == 2)
-            return Center(
-              child: Text(
-                "Group Full",
-                style: TextStyle(
-                    color: kPrimaryColor,
-                    fontSize: 16,
-                    fontWeight: FontWeight.bold),
-              ),
-            );
+            return StreamBuilder(
+                stream: FirebaseFirestore.instance
+                    .collection('Teachers')
+                    .where('Department', isEqualTo: department)
+                    .snapshots(),
+                builder: (BuildContext context, AsyncSnapshot snap) {
+                  if (snap.connectionState == ConnectionState.waiting)
+                    return SpinKitCircle(color: kPrimaryColor);
+                  return Column(
+                    children: [
+                      Container(
+                        child: Padding(
+                          padding: const EdgeInsets.only(top: 10, bottom: 8),
+                          child: Text("Choose Supervisor",
+                              style: GoogleFonts.teko(
+                                color: kPrimaryColor,
+                                fontWeight: FontWeight.bold,
+                                fontSize: 20,
+                              )),
+                        ),
+                      ),
+                      Expanded(
+                        child: SizedBox(
+                          child: ListView.builder(
+                              itemCount: 1,
+                              physics: PageScrollPhysics(
+                                  parent: AlwaysScrollableScrollPhysics()),
+                              controller: PageController(viewportFraction: 1.0),
+                              itemBuilder: (context, index) {
+                                return supervisorList();
+                              }),
+                        ),
+                      ),
+                    ],
+                  );
+                });
+          ////////////////////////////////////////////////////
+
+          // if group is not completed then show students list //
           return StreamBuilder(
             stream: FirebaseFirestore.instance
                 .collection('Students')
@@ -62,18 +95,36 @@ class _DashboardState extends State<Dashboard> {
               if (snap.connectionState == ConnectionState.waiting)
                 return SpinKitCircle(color: kPrimaryColor);
               indexLength = snap.data.docs.length;
-              return SizedBox(
-                child: ListView.builder(
-                    itemCount: indexLength,
-                    physics: PageScrollPhysics(
-                        parent: AlwaysScrollableScrollPhysics()),
-                    controller: PageController(viewportFraction: 1.0),
-                    itemBuilder: (context, index) {
-                      return studentList(snap.data.docs[index], index);
-                    }),
+              return Column(
+                children: [
+                  Container(
+                    child: Padding(
+                      padding: const EdgeInsets.only(top: 10, bottom: 8),
+                      child: Text("Invite Students",
+                          style: GoogleFonts.teko(
+                            color: kPrimaryColor,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                          )),
+                    ),
+                  ),
+                  Expanded(
+                    child: SizedBox(
+                      child: ListView.builder(
+                          itemCount: indexLength,
+                          physics: PageScrollPhysics(
+                              parent: AlwaysScrollableScrollPhysics()),
+                          controller: PageController(viewportFraction: 1.0),
+                          itemBuilder: (context, index) {
+                            return studentList(snap.data.docs[index], index);
+                          }),
+                    ),
+                  ),
+                ],
               );
             },
           );
+          ////////////////////////////////////////////////////
         },
       ),
     );
@@ -112,6 +163,16 @@ class _DashboardState extends State<Dashboard> {
           moreDialog(snapshot);
         },
       ),
+    );
+  }
+
+  supervisorList() {
+    return Center(
+      child: Text("No Supervisor Available",
+          style: GoogleFonts.teko(
+            fontWeight: FontWeight.bold,
+            fontSize: 16,
+          )),
     );
   }
 
